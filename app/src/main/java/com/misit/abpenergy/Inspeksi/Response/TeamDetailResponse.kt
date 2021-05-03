0e9bd26319261a2c17a2e695974fa5a8e898031a
borisreyson <borisreyson@outlook.com>
2021-05-03 08:24:10 +0800

    08.24 3 Mei 2021

--- app/src/main/java/com/misit/abpenergy/Inspeksi/Response/TeamDetailResponse.kt
+++ app/src/main/java/com/misit/abpenergy/Inspeksi/Response/TeamDetailResponse.kt
@@ -1,1 +1,86 @@
-package com.misit.abpenergy.Inspeksi.Responseimport com.google.gson.annotations.SerializedNameclass TeamDetailResponse(    @field:SerializedName("teamInspeksiTemp")    val teamInspeksiTemp: List<TeamInspeksiDetail>? = null)data class TeamInspeksiDetail(    @field:SerializedName("tglentry")    val tglentry: String? = null,    @field:SerializedName("nama_lengkap")    val namaLengkap: String? = null,    @field:SerializedName("id_session")    val idSession: String? = null,    @field:SerializedName("rule")    val rule: String? = null,    @field:SerializedName("perusahaan")    val perusahaan: Int? = null,    @field:SerializedName("section")    val section: String? = null,    @field:SerializedName("idTeam")    val idTeam: Int? = null,    @field:SerializedName("nik")    val nik: String? = null,    @field:SerializedName("password")    val password: String? = null,    @field:SerializedName("id_dept")    val idDept: String? = null,    @field:SerializedName("department")    val department: String? = null,    @field:SerializedName("email")    val email: String? = null,    @field:SerializedName("inc")    val inc: Int? = null,    @field:SerializedName("id_sect")    val idSect: String? = null,    @field:SerializedName("level")    val level: String? = null,    @field:SerializedName("ttd")    val ttd: Any? = null,    @field:SerializedName("photo_profile")    val photoProfile: Any? = null,    @field:SerializedName("nikTeam")    val nikTeam: String? = null,    @field:SerializedName("id_user")    val idUser: Int? = null,    @field:SerializedName("dept")    val dept: String? = null,    @field:SerializedName("idForm")    val idForm: Int? = null,    @field:SerializedName("user_entry")    val userEntry: String? = null,    @field:SerializedName("sect")    val sect: String? = null,    @field:SerializedName("timelog")    val timelog: String? = null,    @field:SerializedName("username")    val username: String? = null,    @field:SerializedName("status")    val status: Int? = null)
+package com.misit.abpenergy.Inspeksi.Response
+
+import com.google.gson.annotations.SerializedName
+
+class TeamDetailResponse(
+    @field:SerializedName("teamInspeksiTemp")
+    var teamInspeksi: List<TeamInspeksiDetail>? = null
+)
+
+data class TeamInspeksiDetail(
+
+    @field:SerializedName("tglentry")
+    var tglentry: String? = null,
+
+    @field:SerializedName("nama_lengkap")
+    var namaLengkap: String? = null,
+
+    @field:SerializedName("id_session")
+    var idSession: String? = null,
+
+    @field:SerializedName("rule")
+    var rule: String? = null,
+
+    @field:SerializedName("perusahaan")
+    var perusahaan: Int? = null,
+
+    @field:SerializedName("section")
+    var section: String? = null,
+
+    @field:SerializedName("idTeam")
+    var idTeam: Int? = null,
+
+    @field:SerializedName("nik")
+    var nik: String? = null,
+
+    @field:SerializedName("password")
+    var password: String? = null,
+
+    @field:SerializedName("id_dept")
+    var idDept: String? = null,
+
+    @field:SerializedName("department")
+    var department: String? = null,
+
+    @field:SerializedName("email")
+    var email: String? = null,
+
+    @field:SerializedName("inc")
+    var inc: Int? = null,
+
+    @field:SerializedName("id_sect")
+    var idSect: String? = null,
+
+    @field:SerializedName("level")
+    var level: String? = null,
+
+    @field:SerializedName("ttd")
+    var ttd: String? = null,
+
+    @field:SerializedName("photo_profile")
+    var photoProfile: String? = null,
+
+    @field:SerializedName("nikTeam")
+    var nikTeam: String? = null,
+
+    @field:SerializedName("id_user")
+    var idUser: Int? = null,
+
+    @field:SerializedName("dept")
+    var dept: String? = null,
+
+    @field:SerializedName("user_entry")
+    var userEntry: String? = null,
+
+    @field:SerializedName("sect")
+    var sect: String? = null,
+
+    @field:SerializedName("timelog")
+    var timelog: String? = null,
+
+    @field:SerializedName("username")
+    var username: String? = null,
+
+    @field:SerializedName("status")
+    var status: Int? = null
+)
