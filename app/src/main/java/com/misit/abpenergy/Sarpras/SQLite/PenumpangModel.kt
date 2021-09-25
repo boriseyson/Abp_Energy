cf0266354d6e881a88659bb9013f1678c964d1cc
borisreyson <borisreyson@outlook.com>
2021-09-26 00:28:44 +0800

    Update From Macbook 26 September 2021

--- app/src/main/java/com/misit/abpenergy/Sarpras/SQLite/PenumpangModel.kt
+++ app/src/main/java/com/misit/abpenergy/Sarpras/SQLite/PenumpangModel.kt
@@ -1,1 +1,1 @@
-package com.misit.abpenergy.Sarpras.SQLiteimport java.io.Serializableclass PenumpangModel:Serializable {    var id:Long?=null    var nik:String?=null    var nama:String?=null    var jabatan:String?=null}
+package com.misit.abpenergy.Sarpras.SQLiteimport java.io.Serializableclass PenumpangModel:Serializable {    var id:Long?=null    var nik:String?=null    var nama:String?=null    var jabatan:String?=null    var penumpang_update:String?=null}
