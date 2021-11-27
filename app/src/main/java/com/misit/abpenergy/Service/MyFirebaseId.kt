8f833516bf1109d22ddcaebeb497d639fa3c9259
Boris <borisreyson@outlook.com>
2021-11-27 17:54:18 +0800

    27 November 2021 Dari Desktop

--- app/src/main/java/com/misit/abpenergy/Service/MyFirebaseId.kt
+++ app/src/main/java/com/misit/abpenergy/Service/MyFirebaseId.kt
@@ -16,6 +16,7 @@ import com.misit.abpenergy.Api.ApiClient
 import com.misit.abpenergy.Api.ApiEndPointTwo
 import com.misit.abpenergy.HomePage.IndexActivity
 import com.misit.abpenergy.R
+import com.misit.abpenergy.Utils.Constants
 import com.misit.abpenergy.Utils.PrefsUtil
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -23,7 +24,12 @@ import kotlinx.coroutines.launch
 import java.lang.Exception
 
 class MyFirebaseId : FirebaseMessagingService() {
+    var nManager:NotificationManager?=null
+    var notificationIntent : Intent?=null
+    lateinit var nBuilder:NotificationCompat.Builder
     override fun onCreate() {
+        createNotificationChannel()
+        notificationIntent = Intent(this, IndexActivity::class.java)
         PrefsUtil.initInstance(this)
         if(PrefsUtil.getInstance().getBooleanState("IS_LOGGED_IN",true)){
             USERNAME = PrefsUtil.getInstance().getStringState(PrefsUtil.USER_NAME,"")
@@ -46,20 +52,19 @@ class MyFirebaseId : FirebaseMessagingService() {
         val id_notif = data["id_notif"]
         if(PrefsUtil.getInstance().getBooleanState("IS_LOGGED_IN",true)) {
             if(tipe=="tenggat_hazard"){
-                firebaseMessage(tipe,uid!!)
+                firebaseMessage()
             }else{
                 notif(title, teks, tipe, uid)
             }
         }
     }
-    private fun firebaseMessage(tipe: String,uid:String){
-        getMessage(tipe,uid)
+    private fun firebaseMessage(){
+        getMessage()
     }
     private fun notifSender(title: String?, body: String?,tipe:String?,uid:String?,id_notif:Int?){
         Log.d("UID", "${uid}")
-        var intent = Intent(this, IndexActivity::class.java)
-        intent.putExtra(IndexActivity.TIPE,tipe)
-        intent.putExtra("UID",uid)
+        notificationIntent?.putExtra(IndexActivity.TIPE,tipe)
+        notificationIntent?.putExtra("UID",uid)
 //        sendMessage(title,body,intent,tipe,id_notif)
     }
     private fun notif(title: String?, body: String?,tipe:String?,uid:String?){
@@ -84,66 +89,34 @@ class MyFirebaseId : FirebaseMessagingService() {
                                             .setContentIntent(pendingIntent)
                                             .setPriority(Notification.PRIORITY_HIGH)
                                             .setStyle(NotificationCompat.BigTextStyle().bigText(body))
-        var nManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        nManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.O){
             var oChannel = NotificationChannel(cId,"Costumer",NotificationManager.IMPORTANCE_HIGH)
-            nManager.createNotificationChannel(oChannel)
+            nManager?.createNotificationChannel(oChannel)
         }
         var id =(1..9999).random()
-        nManager.notify(id,nBuilder.build())
+        nManager?.notify(id,nBuilder.build())
         playNotificationSound(this@MyFirebaseId)
     }
-    private fun sendMessage(title: String?,intent: Intent,id_notif:Int?,pesan:List<String>?) {
+    private fun sendMessage() {
         val GROUP_KEY_WORK_EMAIL = "com.misit.abpenergy.WORK_EMAIL"
-        var iStyle = NotificationCompat.InboxStyle()
-        pesan?.forEach {
-            iStyle.addLine(it)
-        }
-        var pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_ONE_SHOT)
-        var cId = "fcm_default_channel"
-        var dSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-        var nBuilder = NotificationCompat
-            .Builder(this,cId)
+        var pendingIntent = PendingIntent.getActivity(this,0,notificationIntent,PendingIntent.FLAG_ONE_SHOT)
+        nBuilder = NotificationCompat
+            .Builder(this,Constants.CHANNEL_ID)
             .setSmallIcon(R.drawable.abp_white)
             .setColor(R.drawable.abp_blue)
-            .setContentTitle(title)
+            .setGroup(GROUP_KEY_WORK_EMAIL)
+            .setGroupAlertBehavior(NotificationCompat.GROUP_ALERT_CHILDREN)
+            .setGroupSummary(true)
             .setContentText(null)
             .setAutoCancel(true)
-            .setSound(dSoundUri)
             .setContentIntent(pendingIntent)
-            .setPriority(Notification.PRIORITY_HIGH)
-            .setStyle(iStyle)
-            .setGroup(GROUP_KEY_WORK_EMAIL)
-        var nManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+    }
+    private fun getMessage(){
+        sendMessage()
+        var iStyle = NotificationCompat.InboxStyle()
 
-        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.O){
-            var oChannel = NotificationChannel(cId,"Costumer",NotificationManager.IMPORTANCE_HIGH)
-            nManager.createNotificationChannel(oChannel)
-        }
-        var id = 1
-        if(id_notif!=null){
-            id=id_notif
-        }else {
-            id = (1..9999).random()
-        }
-            nManager.notify(id,nBuilder.build())
-        playNotificationSound(this@MyFirebaseId)
-    }
-    private fun playNotificationSound(c:Context) {
-        try {
-            val defSoundURI = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-            val r = RingtoneManager.getRingtone(c,defSoundURI)
-            r.play()
-        }catch (e:Exception){
-            Log.d("ER_Ringtone",e.toString())
-        }
-    }
-    private fun getMessage(tipe:String,uid: String?){
-        var intent = Intent(this, IndexActivity::class.java)
-        intent.putExtra(IndexActivity.TIPE,tipe)
-        intent.putExtra("UID",uid)
-        var pesanku = listOf<String>()
         GlobalScope.launch(Dispatchers.IO){
             val apiEndPoint =
                 ApiClient.getClient(this@MyFirebaseId)?.create(ApiEndPointTwo::class.java)
@@ -152,13 +125,41 @@ class MyFirebaseId : FirebaseMessagingService() {
                 if (response.isSuccessful) {
                     val pesan = response.body()
                     if (pesan != null) {
-
-                        pesanku=pesan!!.message!!
-                        sendMessage("Testing",intent,1,pesanku!!)
+                        pesan?.message?.forEach {
+                            iStyle.addLine(it)
+                        }
+                        nBuilder.setStyle(iStyle)
+                            .setContentTitle("TENGGAT HAZARD")
+                        var id = (1..9999).random()
+                        nManager?.notify(id,nBuilder.build())
                         Log.d("PesanMasuk","${pesan.message}")
                     }
                 }
             }
+        }
+    }
+    private fun createNotificationChannel(){
+        Log.d("FirebaseService","CreateChannel")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val serviceChannel = NotificationChannel(
+                Constants.CHANNEL_ID,
+                "com.misit.abpenergy",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            nManager = getSystemService(
+                NotificationManager::class.java
+            )
+            nManager?.createNotificationChannel(serviceChannel)
+        }
+    }
+
+    private fun playNotificationSound(c:Context) {
+        try {
+            val defSoundURI = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+            val r = RingtoneManager.getRingtone(c,defSoundURI)
+            r.play()
+        }catch (e:Exception){
+            Log.d("ER_Ringtone",e.toString())
         }
     }
     companion object{
